69edea1b179519dcc4abe718b1e3a731d4923e55
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2024-07-12 00:01:15 -0300

    feat: add validator to project

--- gen.go
+++ gen.go
@@ -1,4 +1,5 @@
 package main
 
-//go:generate tern migrate --migrations ./internal/pgstore/migrations/ --config ./internal/pgstore/migrations/tern.conf
+//go:generate goapi-gen -p spec -o internal/api/spec/journey.gen.spec.go internal/api/spec/journey.spec.json
+//go:generate tern migrate -m ./internal/pgstore/migrations/ -c ./internal/pgstore/migrations/tern.conf
 //go:generate sqlc generate -f internal/pgstore/sqlc.yml
